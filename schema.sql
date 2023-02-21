acb8972b8291d16e705bfb89b3a8792ee6d59e8a
george827 <kinyanjuigeorge827@gmail.com>
2023-02-21 20:02:47 +0300

    show sql queries used

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT PRIMARY KEY NOT NULL,
+    name  VARCHAR(100) NOT NULL,
+    date_of_birth DATE,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN,
+    weight_kg Decimal
 );
